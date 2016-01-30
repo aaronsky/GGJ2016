860e327e1a716e231e8e4b31a32ecaf0958c6aca
tcreeds <tcreeds@gmail.com>
2016-01-30 12:48:11 -0500

    Added central lighting effect.

--- Assets/Resources/Scripts/Character.cs
+++ Assets/Resources/Scripts/Character.cs
@@ -12,7 +12,13 @@ public class Character : MonoBehaviour {
 	void Update () {
 	    if (Input.GetKey(KeyCode.RightArrow))
         {
-
+            gameObject.transform.Translate(5 * Time.deltaTime, 0, 0);
         }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-5 * Time.deltaTime, 0, 0);
+        }
+        if (gameObject.transform.position.x > 10)
+            gameObject.transform.position = new Vector3(10, gameObject.transform.position.y);
 	}
 }
--- Assets/Resources/Scripts/SceneObject.cs
+++ Assets/Resources/Scripts/SceneObject.cs
@@ -28,11 +28,11 @@ public class SceneObject : MonoBehaviour {
 
     public void SetData(string imagePath, Vector2 position, string clickText)
     {
-        //spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        /*spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
         Texture2D tex = Resources.Load<Texture2D>(imagePath);
-        //spriteRenderer.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+        spriteRenderer.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
         transform.position.Set(position.x, position.y, 0);
-        text = clickText;
+        text = clickText;*/
     }
 
     public void Unlock()
@@ -65,6 +65,8 @@ public class SceneObject : MonoBehaviour {
 	
     public void OnMouseDown()
     {
+        if (!enabled)
+            return;
         Debug.Log(text);
         foreach (string unlock in unlocks)
         {
