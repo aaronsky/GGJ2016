424e5b3ae35c803e78e402d8b0ec6c92be8d51c6
Chad Weeden <igm013@ad.rit.edu>
2016-01-29 23:19:57 -0500

    Added text class

--- Assets/TextManager.cs
+++ Assets/TextManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class TextManager : MonoBehaviour {
 
+	public TextAsset dialogTextAsset;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
