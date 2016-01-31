1eb710f53ee43de931f28236c3d257e344ef3ae7
Aaron Sky <aaronsky@skyaaron.com>
2016-01-31 13:19:10 -0500

    Added start menu and endings screen

---
+++ Assets/Resources/Scripts/SceneChangeAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeAction : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeSceneTo(string scene)
+    {
+        Application.LoadLevel(scene);
+    }
+}
